13956822b12ef564aebf784e4fa3a5b0bebf7667
grantathon <grant.bartel@gmail.com>
2021-05-10 13:10:58 +0300

    Implemented Crowdsale contracts w/o testing

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,13 @@
-var MyToken = artifacts.require("MyToken.sol");
+var MyToken = artifacts.require("MyToken");
+var MyTokenSale = artifacts.require("MyTokenSale");
 
 module.exports = async (deployer) => {
+  let addr = await web3.eth.getAccounts();
+
   await deployer.deploy(MyToken, 21000000);
+  await deployer.deploy(MyTokenSale, 1, addr[0], MyToken.address);
+
+  // Fund the MyTokenSale contract with minted tokens
+  let instance = await MyToken.deployed();
+  await instance.transfer(MyTokenSale.address, 21000000);
 }
