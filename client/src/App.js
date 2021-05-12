edbc34e0a057a2a553d1553c90f2336806d4765d
grantathon <grant.bartel@gmail.com>
2021-05-12 14:09:40 +0300

    Allow users to buy GBT, set token decimals to 0

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,12 @@ import getWeb3 from "./getWeb3";
 import "./App.css";
 
 class App extends Component {
-  state = { loaded: false, kycAddress: "" };
+  state = {
+    loaded: false,
+    kycAddress: "",
+    tokenSaleAddress: null,
+    userTokens: 0
+  };
 
   componentDidMount = async () => {
     try {
@@ -35,9 +40,14 @@ class App extends Component {
         Kyc.networks[this.networkId] && Kyc.networks[this.networkId].address,
       );
 
+      this.listenToTokenTransfer();
+
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ loaded: true });
+      this.setState(
+        {loaded: true, tokenSaleAddress: MyTokenSale.networks[this.networkId].address},
+        this.updateUserTokens
+      );
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -46,6 +56,19 @@ class App extends Component {
       console.error(error);
     }
   };
+
+  updateUserTokens = async () => {
+    let userTokens = await this.tokenInstance.methods.balanceOf(this.accounts[0]).call();
+    this.setState({userTokens: userTokens});
+  }
+
+  listenToTokenTransfer = () => {
+    this.tokenInstance.events.Transfer({to: this.accounts[0]}).on("data", this.updateUserTokens);
+  }
+
+  handleBuyTokens = async () => {
+    await this.tokenSaleInstance.methods.buyTokens(this.accounts[0]).send({from: this.accounts[0], value: this.web3.utils.toWei("1", "wei")});
+  }
 
   handleInputChange = (event) => {
     const target = event.target;
@@ -74,6 +97,10 @@ class App extends Component {
           Address to allow: <input type="text" name="kycAddress" value={this.state.kycAddress} onChange={this.handleInputChange} />
         </p>
         <button type="button" onClick={this.handleKycWhitelisting}>Add to Whitelist</button>
+        <h2>Buy Tokens</h2>
+        <p>If you want to buy tokens, send Wei to this address: {this.state.tokenSaleAddress}</p>
+        <p>You currently have: {this.state.userTokens} Grant Tokens</p>
+        <button type="button" onClick={this.handleBuyTokens}>Buy More Tokens</button>
       </div>
     );
   }
--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -3,7 +3,11 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Grant Token", "GBT") {
-        _mint(msg.sender, initialSupply);
-    }
+  constructor(uint256 initialSupply) ERC20("Grant Token", "GBT") {
+      _mint(msg.sender, initialSupply);
+  }
+
+  function decimals() public view override returns (uint8) {
+    return 0;
+  }
 }
